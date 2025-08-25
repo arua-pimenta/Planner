6970563a3532233b9a1d7c000eeb4ddfe8da16da
sharann-del <sharannmanojkumar@gmail.com>
2025-08-25 20:14:14 +0530

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Sharann M on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlannerTests/PlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerTests.swift
+//  PlannerTests
+//
+//  Created by Sharann M on 25/08/25.
+//
+
+import Testing
+@testable import Planner
+
+struct PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
